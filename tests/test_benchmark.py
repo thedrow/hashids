e0cb97d19750692dd3a74272757ce2e2efdf6055
Omer Katz <omer.drow@gmail.com>
2016-08-31 10:25:10 +0300

    Benchmark more options.

--- tests/test_benchmark.py
+++ tests/test_benchmark.py
@@ -1,44 +1,45 @@
-import gc
 import pytest
 from hashids import Hashids
 from hashids_cffi import Hashids as HashidsCFFI
 
 
 @pytest.mark.benchmark(group='encode', warmup=True, min_rounds=10, warmup_iterations=1000000)
-def test_encode_cffi(benchmark):
-    gc.collect()
-
+@pytest.mark.parametrize("min_length", (0, 4, 8, 12, 24, 48, 120))
+def test_encode_cffi(benchmark, min_length):
     @benchmark
     def bench():
-        h = HashidsCFFI()
+        h = HashidsCFFI(min_length=min_length)
         h.encode(1)
 
 
 @pytest.mark.benchmark(group='encode', warmup=True, min_rounds=10, warmup_iterations=1000000)
-def test_encode(benchmark):
-    gc.collect()
-
+@pytest.mark.parametrize("min_length", (0, 4, 8, 12, 24, 48, 120))
+def test_encode(benchmark, min_length):
     @benchmark
     def bench():
-        h = Hashids()
+        h = Hashids(min_length=min_length)
         h.encode(1)
 
 
 @pytest.mark.benchmark(group='decode', warmup=True, min_rounds=10, warmup_iterations=1000000)
-def test_decode_cffi(benchmark):
-    gc.collect()
-
+@pytest.mark.parametrize("min_length", (0, 4, 8, 12, 24, 48, 120))
+@pytest.mark.parametrize("hashid", ('jR', 'ejRe', 'olejRejN', '2VolejRejNmG', 'L39J4q2VolejRejNmGQBW71g',
+                                    'KAyZnxO60XzkL39J4q2VolejRejNmGQBW71gPv58RYDMrpwE',
+                                    'W9jOVvKy2G7qgRLGgV6QYRoBlrkyXZ5A43v2KAyZnxO60XzkL39J4q2VolejRejNmGQBW71gPv58RYDMrpwEO8z9jD0MEmNJqK7nw1xWPpBxkm5A4nYzXoMZ'))
+def test_decode_cffi(benchmark, min_length, hashid):
     @benchmark
     def bench():
-        h = HashidsCFFI()
-        h.decode('gLNBk')
+        h = HashidsCFFI(min_length=min_length)
+        h.decode(hashid)
 
 
 @pytest.mark.benchmark(group='decode', warmup=True, min_rounds=10, warmup_iterations=1000000)
-def test_decode(benchmark):
-    gc.collect()
-
+@pytest.mark.parametrize("min_length", (0, 4, 8, 12, 24, 48, 120))
+@pytest.mark.parametrize("hashid", ('jR', 'ejRe', 'olejRejN', '2VolejRejNmG', 'L39J4q2VolejRejNmGQBW71g',
+                                    'KAyZnxO60XzkL39J4q2VolejRejNmGQBW71gPv58RYDMrpwE',
+                                    'W9jOVvKy2G7qgRLGgV6QYRoBlrkyXZ5A43v2KAyZnxO60XzkL39J4q2VolejRejNmGQBW71gPv58RYDMrpwEO8z9jD0MEmNJqK7nw1xWPpBxkm5A4nYzXoMZ'))
+def test_decode(benchmark, min_length, hashid):
     @benchmark
     def bench():
-        h = Hashids()
-        h.decode('gLNBk')
+        h = Hashids(min_length=min_length)
+        h.decode(hashid)
