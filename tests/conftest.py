221ce9f88fdf353e93ca77bf736a144cd5449350
Omer Katz <omer.drow@gmail.com>
2016-09-20 15:32:23 +0300

    Ignore hypothesis settings when not running the job in CI.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,16 @@
 import os
 from hypothesis import settings, Verbosity
-from tests.examples_database import ThreadedSQLiteExampleDatabase
 
-hypothesis_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../.hypothesis')
-settings.register_profile("ci", settings(max_examples=100000, verbosity=Verbosity.verbose,
-                                         database=ThreadedSQLiteExampleDatabase(max_workers=2, path=hypothesis_path)))
-settings.register_profile("default", settings(max_examples=10000, verbosity=Verbosity.verbose,
-                                              database=ThreadedSQLiteExampleDatabase(max_workers=4)))
-settings.load_profile(os.getenv(u'HYPOTHESIS_PROFILE', 'default'))
+try:
+    from tests.examples_database import ThreadedSQLiteExampleDatabase
+
+    hypothesis_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../.hypothesis')
+    settings.register_profile("ci", settings(max_examples=100000, verbosity=Verbosity.verbose,
+                                             database=ThreadedSQLiteExampleDatabase(max_workers=2,
+                                                                                    path=hypothesis_path)))
+    settings.register_profile("default", settings(max_examples=10000, verbosity=Verbosity.verbose,
+                                                  database=ThreadedSQLiteExampleDatabase(max_workers=4)))
+    settings.load_profile(os.getenv(u'HYPOTHESIS_PROFILE', 'default'))
+except ImportError:
+    # Hypothesis is not being used in this job so we don't care
+    pass
