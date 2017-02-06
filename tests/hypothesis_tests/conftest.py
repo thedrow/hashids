0f8fd48855fc1e42e8a47a887002f448c2f54c83
Omer Katz <omer.drow@gmail.com>
2017-02-06 22:25:59 +0200

    Remove custom examples database.

--- tests/hypothesis_tests/conftest.py
+++ tests/hypothesis_tests/conftest.py
@@ -2,14 +2,9 @@ import os
 from hypothesis import settings, Verbosity
 
 try:
-    from tests.hypothesis_tests.examples_database import ThreadedSQLiteExampleDatabase
-
     hypothesis_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../.hypothesis')
-    settings.register_profile("ci", settings(max_examples=100000, verbosity=Verbosity.verbose,
-                                             database=ThreadedSQLiteExampleDatabase(max_workers=2,
-                                                                                    path=hypothesis_path)))
-    settings.register_profile("default", settings(max_examples=10000, verbosity=Verbosity.verbose,
-                                                  database=ThreadedSQLiteExampleDatabase(max_workers=4)))
+    settings.register_profile("ci", settings(max_examples=100000, verbosity=Verbosity.verbose))
+    settings.register_profile("default", settings(max_examples=10000, verbosity=Verbosity.verbose))
     settings.load_profile(os.getenv(u'HYPOTHESIS_PROFILE', 'default'))
 except ImportError:
     # Hypothesis is not being used in this job so we don't care
